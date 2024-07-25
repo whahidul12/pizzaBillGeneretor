31df86d98089f03f10422a3691eec22c808e8913
md-whahid <mdislamwhahid@gmail.com>
2024-07-25 21:23:10 +0600

    bangla block

---
+++ MNB.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MNB {
+  public static void main(String[] args) {
+    List<Integer> list = new ArrayList<>();
+    list.add(12);
+    list.add(12);
+    list.add(12);
+    list.add(12);
+    list.add(12);
+    System.out.println(list);
+
+  }
+
+}
