2730b8c81825dad5cfdeb2e28648b9e28900c8f8
md-whahid <mdislamwhahid@gmail.com>
2024-07-26 23:54:24 +0600

    ArrayKist 2.0

--- Arraylist.java
+++ Arraylist.java
@@ -3,21 +3,12 @@ import java.util.Scanner;
 
 public class Arraylist {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
 
-    Scanner dd = new Scanner(System.in);
-
-    int a = dd.nextInt();
-    double b = dd.nextDouble();
-
-    dd.nextLine();
-    String c = dd.nextLine();
-    dd.close();
-
-    System.out.println("String: " + c);
-    System.out.println("Double: " + b);
-    System.out.println("Integer: " + a);
-    System.out.printf(c, args);
+    String str = "new world is this";
+    String[] strArr = str.split(" ");
+    for (String n : strArr) {
+      System.out.println(n);
+    }
   }
-
 }
