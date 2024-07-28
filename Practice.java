8fd955e27888e1abc51018473469be9065199932
md-whahid <mdislamwhahid@gmail.com>
2024-07-29 00:06:23 +0600

    my mind stop working

---
+++ Practice.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Practice {
+
+  public static void main(String[] args) {
+
+    int a, b, n;
+    Scanner scanner = new Scanner(System.in);
+    int q = scanner.nextInt();
+    for (int i = 0; i < q; i++) {
+      a = scanner.nextInt();
+      b = scanner.nextInt();
+      n = scanner.nextInt();
+    }
+    scanner.close();
+    for (int i = 0; i < n; i++) {
+      for(int j = 0; j <= i; j++){
+        
+      }
+      double r = (a + (Math.pow(2, i)) * (OP) * b);
+
+    }
+  }
+}
